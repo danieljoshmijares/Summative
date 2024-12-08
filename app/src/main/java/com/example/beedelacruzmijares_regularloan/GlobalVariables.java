e37c6cfbb633454bf277f36c1c2e9a75190c3200
MintyShiva <johngabrieldelacruz28@gmail.com>
2024-12-09 01:50:18 +0800

    play Limbus Company on the Google Play Store and Steam

--- app/src/main/java/com/example/beedelacruzmijares_regularloan/GlobalVariables.java
+++ app/src/main/java/com/example/beedelacruzmijares_regularloan/GlobalVariables.java
@@ -4,21 +4,74 @@ import static java.lang.Double.parseDouble;
 
 public class GlobalVariables {
     public static String empid, empname, date, password;
-    public static double loanamount, servicecharge, cash, months, interest, sixmonpay;
-    public static double loanint;
+    public static double loanamount, servicecharge, cash, months, interest, sixmonpay = 0;
+    public static double loanint, loantotal, monamort = 0;
+    public static double salary, regloanamount, regloanint, takehome, regservcharge, regmonamort = 0;
 
-    public double getServicecharge(){
+    //Emergency Loan
+    public double getEmergencyServicecharge(){
     servicecharge = loanamount * 0.01;
     return servicecharge;
     }
-
-    public static double getCash() {
+    public static double getEmergencyCash() {
         cash = loanamount + servicecharge;
         return cash;
     }
-    public static double getEmergencyLoan() {
+    public static double getEmergencyLoanPayable() {
+        interest = loanamount * 0.006;
         sixmonpay = (loanamount + servicecharge + interest)/months;
         return sixmonpay;
+    }
+
+    //Total Loan
+    public static double getTotalLoanInterest() {
+        loanint = loanamount * months;
+        if (months >= 1 && months <= 6) {
+            loanint *= 0.006;
+        } else if (months >= 7 && months <= 12) {
+            loanint *= 0.0062;
+        } else if (months >= 13 && months <= 18) {
+            loanint *= 0.0065;
+        }
+        return loanint;
+    }
+    public static double getTotalLoanAmount() {
+        loantotal = loanamount + loanint;
+        return loantotal;
+    }
+    public static double getMothlyAmort(){
+        monamort = loantotal/months;
+        return loantotal;
+    }
+
+    //Regular Loan
+    public static double getRegularLoanAmount() {
+        regloanamount = salary * 2.5;
+        return regloanamount;
+    }
+    public static double getRegularLoanInterest() {
+        regloanint = regloanamount * months;
+        if (months >= 1 && months <= 5) {
+            regloanint *= 0.0062;
+        } else if (months >= 6 && months <= 10) {
+            regloanint *= 0.0065;
+        } else if (months >= 11 && months <= 15) {
+            regloanint *= 0.0068;
+        } else if (months >= 16 && months <= 20) {
+            regloanint *= 0.0075;
+        } else if (months >= 21 && months <= 24) {
+            regloanint *= 0.0080;
+        }
+        return regservcharge;
+    }
+    public static double getTakeHome() {
+        regservcharge = regloanamount * 0.02;
+        takehome = regloanamount - (regloanint + regservcharge);
+        return takehome;
+    }
+    public static double getRegmonamort() {
+        regmonamort = takehome/months;
+        return regmonamort;
     }
 
 }
--- app/src/main/java/com/example/beedelacruzmijares_regularloan/RegularLoan.java
+++ app/src/main/java/com/example/beedelacruzmijares_regularloan/RegularLoan.java
@@ -1,7 +1,10 @@
 package com.example.beedelacruzmijares_regularloan;
 
+import static java.lang.Double.parseDouble;
+
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Intent;
 import androidx.activity.EdgeToEdge;
@@ -15,15 +18,27 @@ public class RegularLoan extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_regular_loan);
 
+        EditText salarytxt = findViewById(R.id.regular_salary);
+        EditText monthstxt = findViewById(R.id.regular_months);
         Button btnRegularApplyLoan = findViewById(R.id.apply_regular_btn);
         btnRegularApplyLoan.setOnClickListener(v -> {
-            // Show a toast
-            Toast.makeText(this, "Loan applied successfully!", Toast.LENGTH_SHORT).show();
+            if (monthstxt.length() == 0 || salarytxt.length() == 0) {
+                Toast.makeText(RegularLoan.this, "Please fill out the needed form to proceed.", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                double monthsnum = parseDouble(monthstxt.getText().toString());
+                if(monthsnum < 1 || monthsnum > 24){
+                    Toast.makeText(this, "Please input the correct month number", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    double salarynum = parseDouble(salarytxt.getText().toString());
 
-            // Go back to the UserHomePage
-            Intent intent = new Intent(RegularLoan.this, UserHomePage.class);
-            startActivity(intent);
-            finish(); // Optional: Finish the current activity to prevent going back
-        });
+                    // Show a toast
+                    Toast.makeText(this, "Loan applied successfully!", Toast.LENGTH_SHORT).show();
+                    // Go back to the UserHomePage
+                    Intent intent = new Intent(RegularLoan.this, UserHomePage.class);
+                    startActivity(intent);
+                    finish(); // Optional: Finish the current activity to prevent going back
+                }}});
     }
 }
